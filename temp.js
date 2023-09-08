fd8d7596f38d37df41803ea3a13c3330643957a7
Vishnu Narugondla <109599759+vishnu6960@users.noreply.github.com>
2023-09-08 22:17:59 +0530

    Update temp.js

--- temp.js
+++ temp.js
@@ -8,37 +8,37 @@ function convert(){
     var to=document.getElementById("units2").value
     if(from=="Celsius" && to=="Fahrenheit"){
         output=(intValue * 9/5) + 32
-        Result.innerHTML=output
+        Result.innerHTML=output.toFixed(2)
         console.log(output)
     }
     if(from=="Celsius" && to=="Kelvin"){
         output=intValue + 273.15
-        Result.innerHTML=output
+        Result.innerHTML=output.toFixed(2)
         console.log(output)
     }
     if(from=="Fahrenheit" && to=="Celsius"){
         output=(intValue - 32) * 5/9
-        Result.innerHTML=output
+        Result.innerHTML=output.toFixed(2)
         console.log(output)
     }
     if(from=="Fahrenheit" && to=="Kelvin"){
         output=(intValue - 32) * 5/9 +273.15
-        Result.innerHTML=output
+        Result.innerHTML=output.toFixed(2)
         console.log(output)
     }
     if(from=="Kelvin" && to=="Fahrenheit"){
         output=(intValue - 273.15) * 9/5 + 32
-        Result.innerHTML=output
+        Result.innerHTML=output.toFixed(2)
         console.log(output)
     }
     if(from=="Kelvin" && to=="Celsius"){
         output=intValue - 273.15
-        Result.innerHTML=output
+        Result.innerHTML=output.toFixed(2)
         console.log(output)
     }
     if((from=="Celsius" && to=="Celsius") || (from=="Fahrenheit" && to=="Fahrenheit") || (from=="Kelvin" && to=="Kelvin") ){
         output=intValue
-        Result.innerHTML=output
+        Result.innerHTML=output.toFixed(2)
         console.log(intValue)
     }
     
